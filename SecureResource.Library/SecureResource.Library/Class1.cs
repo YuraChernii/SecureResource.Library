3fb324ab9f85f224b40add356c24465da4853326
YuraChernii <mik373737373737@gmail.com>
2022-07-05 14:54:04 -0700

    added yml

---
+++ SecureResource.Library/SecureResource.Library/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SecureResource.Library
+{
+    public class Class1
+    {
+        public int Return1()
+        {
+            Console.WriteLine(1);
+            return 1;
+        }
+    }
+}
